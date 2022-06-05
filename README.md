d55f0c202d8984eef8785d4a91594cc192449ee8
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2022-06-05 19:43:56 +0000

    Hello World

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chess-engine
+
 A Simple Chess Engine written in C++.
+My First C++ Project so will be Basic.
---
+++ src/Main.cc
@@ -0,0 +1,10 @@
+// hello world program
+
+#include <iostream>
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
