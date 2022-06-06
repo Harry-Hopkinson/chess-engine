4baa804099cdd29f543b9c6901a1232fc4d83495
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2022-06-06 09:33:14 +0000

    Print the Board

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A Simple Chess Engine written in C++.
 My First C++ Project so will be Basic.
+
+https://www.codeproject.com/Articles/1214018/Chess-Console-Game-in-Cplusplus
--- src/Board.cc
+++ src/Board.cc
@@ -1,6 +1,40 @@
 #include "Board.h"
 #include <iostream>
 
-int main() {
-  std::cout << WHITE_SQUARE << std::endl;
+void printBoard(Game & game) {
+  cout << "   A     B     C     D     E     F     G     H\n\n";
+
+  for (int iLine = 7; iLine >= 0; iLine--) {
+    if (iLine % 2 == 0) {
+      printLine(iLine, BLACK_SQUARE, WHITE_SQUARE, game);
+    } else {
+      printLine(iLine, WHITE_SQUARE, BLACK_SQUARE, game);
+    }
+  }
+}
+
+void printLine(int iLine, int iColour1, int iColour2, Game & game) {
+  int CELL = 6;
+
+  for (int subLine = 0; subLine < CELL / 2; subLine++) {
+    for (int iPair = 0; iPair < 4; iPair++) {
+      for (int subColumn = 0; subColumn < CELL; subColumn++) {
+        if (subLine == 1 && subColumn == 3) {
+          cout << char(game.getPieceAtPosition(iLine, iPair * 2) != 0x20 ?
+            game.getPieceAtPosition(iLine, iPair * 2) : iColour1);
+        } else {
+          cout << char(iColour1);
+        }
+      }
+
+      for (int subColumn = 0; subColumn < CELL; subColumn++) {
+        if (subLine == 1 && subColumn == 3) {
+          cout << char(game.getPieceAtPosition(iLine, iPair * 2 + 1) != 0x20 ?
+            game.getPieceAtPosition(iLine, iPair * 2 + 1) : iColour2);
+        } else {
+          cout << char(iColour2);
+        }
+      }
+    }
+  }
 }
